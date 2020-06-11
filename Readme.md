bce39de1a96df35a13413e4afc2e39a12ec5aea4
geffzhang <geffzhang@weyhd.com>
2020-06-11 13:05:19 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Install Global Tools
+```
+dotnet tool install --global Microsoft.Tye --version 0.2.0-alpha.20258.3
+```
+
+# Using Tye Cli to Exec All Projects 
+```
+tye run
+```
+or
+
+# Using dotnet   Cli to Exec All Projects 
+```
+dotnet run -p ./Api1/Api1.csproj
+dotnet run -p ./Api2/Api2.csproj
+dotnet run -p ./ReverseProxy/ReverseProxy.csproj
+```
+
+# Application Url
+- http://localhost:5020/api1/WeatherForecast
+- http://localhost:5020/api2/WeatherForecast
+
+
+# Reference
+- https://github.com/dotnet/tye
+- https://github.com/microsoft/reverse-proxy
---
+++ ReverseProxy/Startup.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace ReverseProxy {
+    public class Startup {
+
+        public Startup (IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddControllers ();
+            services.AddReverseProxy ()
+                .LoadFromConfig (Configuration.GetSection ("ReverseProxy"));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger) {
+            //  app.UseHttpsRedirection ();
+
+            app.UseRouting ();
+            app.UseAuthorization ();
+            app.UseEndpoints (endpoints => {
+
+                endpoints.MapControllers ();
+                endpoints.MapReverseProxy (proxyPipeline => {
+
+                    proxyPipeline.UseProxyLoadBalancing ();
+                });
+               logger.LogWarning (Configuration.GetServiceUri ("api2")?.ToString());
+                              logger.LogWarning (Configuration.GetServiceUri ("api1")?.ToString());
+
+
+            });
+
+        }
+    }
+}
